5c438b4667a0fa06f4a3e280a1ebdb3c392fbb0b
Rakib544 <md.rakib10122003@gmail.com>
2021-12-30 20:52:03 +0600

    practise with number, string, boolean

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello world! I am learning Typescript");
+// Practice with number, string and boolean
+function calculateResult(number1, number2, greet, isLoggedIn) {
+    if (isLoggedIn) {
+        return greet + " " + (number1 + number2);
+    }
+    else {
+        return "Not authorized to see the result";
+    }
+}
+console.log(calculateResult(90, 89, "Your result is - ", false));
--- script.ts
+++ script.ts
@@ -1,1 +1,16 @@
-console.log("Hello world! I am learning Typescript! Yeah");
+// Practice with number, string and boolean
+
+function calculateResult(
+  number1: number,
+  number2: number,
+  greet: string,
+  isLoggedIn: boolean
+): string {
+  if (isLoggedIn) {
+    return `${greet} ${number1 + number2}`;
+  } else {
+    return "Not authorized to see the result";
+  }
+}
+
+console.log(calculateResult(90, 89, "Your result is - ", false));
