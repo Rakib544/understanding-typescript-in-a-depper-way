d7a34e92a01151ef51dcbac0a9d1d23a1ed4fc44
Rakib544 <md.rakib10122003@gmail.com>
2022-01-04 23:17:22 +0600

    new commit

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 // Practice with number, string and boolean
-function calculateResult(number1, number2, greet, isLoggedIn) {
-    if (isLoggedIn) {
-        return greet + " " + (number1 + number2);
+// function calculateResult(
+//   number1: number,
+//   number2: number,
+//   greet: string,
+//   isLoggedIn: boolean
+// ): string {
+//   if (isLoggedIn) {
+//     return `${greet} ${number1 + number2}`;
+//   } else {
+//     return "Not authorized to see the result";
+//   }
+// }
+// console.log(calculateResult(90, 89, "Your result is - ", false));
+// Practice with union data type
+function combineInputs(input1, input2) {
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        return input1 + input2;
     }
     else {
-        return "Not authorized to see the result";
+        return input1.toString() + input2.toString();
     }
 }
-console.log(calculateResult(90, 89, "Your result is - ", false));
+console.log(combineInputs(1, 2));
+console.log(combineInputs("hello", "world"));
--- script.ts
+++ script.ts
@@ -1,16 +1,28 @@
 // Practice with number, string and boolean
 
-function calculateResult(
-  number1: number,
-  number2: number,
-  greet: string,
-  isLoggedIn: boolean
-): string {
-  if (isLoggedIn) {
-    return `${greet} ${number1 + number2}`;
+// function calculateResult(
+//   number1: number,
+//   number2: number,
+//   greet: string,
+//   isLoggedIn: boolean
+// ): string {
+//   if (isLoggedIn) {
+//     return `${greet} ${number1 + number2}`;
+//   } else {
+//     return "Not authorized to see the result";
+//   }
+// }
+
+// console.log(calculateResult(90, 89, "Your result is - ", false));
+
+// Practice with union data type
+function combineInputs(input1: number | string, input2: number | string) {
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    return input1 + input2;
   } else {
-    return "Not authorized to see the result";
+    return input1.toString() + input2.toString();
   }
 }
 
-console.log(calculateResult(90, 89, "Your result is - ", false));
+console.log(combineInputs(1, 2));
+console.log(combineInputs("hello", "world"));
