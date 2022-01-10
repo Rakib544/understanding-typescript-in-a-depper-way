14a222b167915dbc184b5008bf1b8a3eee2be885
Rakib544 <md.rakib10122003@gmail.com>
2022-01-10 22:24:51 +0600

    practice with inheritance

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Department = /** @class */ (function () {
     function Department(id, name) {
         this.id = id;
@@ -19,7 +34,38 @@ var Department = /** @class */ (function () {
     };
     return Department;
 }());
-var computer = new Department(66, "computer");
+// super(); when you use your own constructor which is inherit from another class you have to use super() keyword. And this super() calls the main constructor.
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, name, admins) {
+        var _this = _super.call(this, id, name) || this;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var computer = new ITDepartment(66, "computer", ["sakil"]);
 computer.addSubjects("Data structure and algorithm");
 computer.showSubjects();
 computer.describe();
+console.log(computer);
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, name, reports) {
+        var _this = _super.call(this, id, name) || this;
+        _this.reports = reports;
+        return _this;
+    }
+    AccountingDepartment.prototype.addReport = function (report) {
+        this.reports.push(report);
+    };
+    AccountingDepartment.prototype.getReports = function () {
+        console.log("reports: " + this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var accounting = new AccountingDepartment(88, "Accounting", []);
+accounting.addReport("Hello this is report");
+accounting.getReports();
+accounting.addSubjects("Business Organization and communication");
+accounting.showSubjects();
--- script.ts
+++ script.ts
@@ -20,8 +20,39 @@ class Department {
   }
 }
 
-const computer = new Department(66, "computer");
+// super(); when you use your own constructor which is inherit from another class you have to use super() keyword. And this super() calls the main constructor.
+
+class ITDepartment extends Department {
+  constructor(id: number | string, name: string, public admins: string[]) {
+    super(id, name);
+  }
+}
+
+const computer = new ITDepartment(66, "computer", ["sakil"]);
 
 computer.addSubjects("Data structure and algorithm");
 computer.showSubjects();
 computer.describe();
+
+console.log(computer);
+
+class AccountingDepartment extends Department {
+  constructor(id: number | string, name: string, public reports: string[]) {
+    super(id, name);
+  }
+
+  addReport(report: string) {
+    this.reports.push(report);
+  }
+
+  getReports() {
+    console.log(`reports: ${this.reports}`);
+  }
+}
+
+const accounting = new AccountingDepartment(88, "Accounting", []);
+
+accounting.addReport("Hello this is report");
+accounting.getReports();
+accounting.addSubjects("Business Organization and communication");
+accounting.showSubjects();
